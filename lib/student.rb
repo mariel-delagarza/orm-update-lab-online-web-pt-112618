69427e268b59cf895cfd74a558999a8ec1a54680
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-18 20:04:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,8 +57,14 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
-    DB[:conn].execute(sql, name).map do |row|
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
   end
