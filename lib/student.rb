542fdbde6ff1115701d47759f0df38f308388399
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-18 19:54:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
-    student
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
