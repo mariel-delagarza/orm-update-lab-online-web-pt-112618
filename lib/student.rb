2cb8970f642f536eca38b969d461901d836beca4
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-18 19:16:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,15 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
 
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
